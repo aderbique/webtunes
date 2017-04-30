f179dd2a476d021de05e618b7b35833c7a9f318c
Austin Derbique <austin@localhost.localdomain>
2017-04-30 15:20:00 -0600

    deleted myenv

--- webtunes/myenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/austin/programming/cs2610/finalproject/webtunes/myenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
